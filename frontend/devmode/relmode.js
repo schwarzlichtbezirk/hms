2841e5196bba47be2671c6fa4ca7f909d9e0d14f
schwarzlichtbezirk <schwarzlichtbezirk@gmail.com>
2021-03-18 01:42:39 +0300

    leaflet-control-attribution.

--- frontend/devmode/relmode.js
+++ frontend/devmode/relmode.js
@@ -3,7 +3,7 @@
 // This file is included for release mode linkage
 
 const buildvers = "0.7.2";
-const builddate = "2021.03.12";
+const builddate = "2021.03.18";
 const devmode = false;
 
 const traceajax = () => undefined;
