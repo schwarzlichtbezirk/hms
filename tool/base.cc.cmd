768e6eaac0d790fc6a2af977c32f6766344db794
schwarzlichtbezirk <schwarzlichtbezirk@gmail.com>
2021-08-10 23:50:16 +0300

    bootstrap bundle removed.

--- tool/base.cc.cmd
+++ tool/base.cc.cmd
@@ -2,14 +2,8 @@
 cd /d %GOPATH%/src/github.com/schwarzlichtbezirk/hms/frontend
 
 java -jar %~d0/tools/closure-compiler.jar^
- --js plugin/popper.min.js^
- --js plugin/bootstrap.min.js^
- --strict_mode_input^
- --js_output_file build/base.bundle.js^
- 
-java -jar %~d0/tools/closure-compiler.jar^
  --js plugin/leaflet.js^
  --js plugin/leaflet.markercluster.js^
  --js plugin/sha256.min.js^
  --strict_mode_input^
- --js_output_file build/app.bundle.js^
+ --js_output_file build/app.bundle.js
